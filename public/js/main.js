1d7a47e5938895d2a22efec2ede7adf359de2477
Merav Corem <merav31193@gmail.com>
2022-01-17 12:34:38 +0200

    Format, scrollIntoView, const

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const userList = document.getElementById('users');
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const userList = document.getElementById("users");
 
 // Get username from URL
 const { username } = Qs.parse(location.search, {
@@ -9,56 +9,57 @@ const { username } = Qs.parse(location.search, {
 
 const socket = io();
 
-// Join chat
-socket.emit('joinChat', { username });
-
-// Get users
-socket.on('chatUsers', ({ users }) => {
-  outputUsers(users);
-});
-
-// Message from server
-socket.on('message', (message) => {
-  console.log(message);
-  outputMessage(message);
-
-  // Scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
-
-// Message submit
-chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // Get message text
-  let msg = e.target.elements.msg.value;
-
-  // Emit message to server
-  socket.emit('chatMessage', msg);
-
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
-});
-
 // Output message to DOM
 function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  div.innerHTML = `<p class="meta">${message.userName}<span> ${message.time}</span></p>
+  const messageElm = document.createElement("div");
+  messageElm.classList.add("message");
+  messageElm.innerHTML = `
+  <p class="meta">
+    ${message.userName}<span> ${message.time}</span>
+  </p>
   <p class="text">
-   ${message.text}
+    ${message.text}
   </p>`;
-  document.querySelector('.chat-messages').appendChild(div);
-
+  document.querySelector(".chat-messages").appendChild(messageElm);
+  messageElm.scrollIntoView();
 }
 
 // Add users to DOM
 function outputUsers(users) {
-  userList.innerHTML = '';
+  userList.innerHTML = "";
   users.forEach((user) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.innerText = user.username;
     userList.appendChild(li);
   });
 }
+
+// Join chat
+socket.emit("joinChat", { username });
+
+// Get users
+socket.on("chatUsers", ({ users }) => {
+  outputUsers(users);
+});
+
+// Message from server
+socket.on("message", (message) => {
+  outputMessage(message);
+});
+
+// Message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Get message text
+  const msg = e.target.elements.msg.value;
+
+  // Emit message to server
+  socket.emit("chatMessage", msg);
+
+  // Clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+
--- utils/users.js
+++ utils/users.js
@@ -1,35 +1,35 @@
 const users = [];
 
 //Join user to chat
-function userJoin(id, username){
-    const user = {id, username };
+function userJoin(id, username) {
+  const user = { id, username };
 
-    users.push(user);
+  users.push(user);
 
-    return user;
+  return user;
 }
 
 // Get current user
-function getCurrentUser(id){
-    return users.find(user => user.id === id);
+function getCurrentUser(id) {
+  return users.find((user) => user.id === id);
 }
 
 //User leaves chat
-function userLeave(id){
-    const index = users.findIndex(user => user.id === id);
+function userLeave(id) {
+  const index = users.findIndex((user) => user.id === id);
 
-    if(index !== -1){
-        return users.splice(index, 1)[0];
-    }
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
 }
 
-function getAllUsers(){
-    return users;
+function getAllUsers() {
+  return users;
 }
 
 module.exports = {
-    userJoin,
-    getCurrentUser,
-    userLeave,
-    getAllUsers
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getAllUsers,
 };
